3ce467ceeaf87916ebb2dd856910979b30a90521
odellla <odellla@live.unc.edu>
2019-04-23 23:10:59 -0400

    Added scoreboard functionality

---
+++ Assets/Scripts/BoatCollider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoatCollider : MonoBehaviour
+{
+
+//	Text t;
+	public static int scoreValue = 0;
+	Text score;
+	
+	void Start(){
+		score = GetComponent<Text>();
+	}
+	void Update(){
+		score.text = "Damage: $" + scoreValue;
+	}
+	
+	void OnCollisionEnter (Collision col)
+    {
+
+	//t = canvas.GetComponent<Text>();
+			Debug.Log(col.gameObject.name);
+        if(col.gameObject.name != "Seawall")
+        {
+			BoatCollider.scoreValue++;
+			//t.text = "Score: " + score;
+			//Destroy(col.gameObject);
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Random;
+
+public class CollisionDetector : MonoBehaviour{
+    // Start is called before the first frame update
+void OnCollisionEnter (Collision col){
+
+	//t = canvas.GetComponent<Text>();
+			Debug.Log(col.gameObject.name);
+        if(col.gameObject.name != "Seawall"){
+			
+			if(col.relativeVelocity.magnitude >= 1){
+				BoatCollider.scoreValue = BoatCollider.scoreValue + ((int)Random.Range(10,50)* (int)col.relativeVelocity.magnitude);
+			}else if (col.relativeVelocity.magnitude >= 5){
+				BoatCollider.scoreValue = BoatCollider.scoreValue + ((int)Random.Range(50,100)* (int)col.relativeVelocity.magnitude);
+			}else if(col.relativeVelocity.magnitude < 5){
+				BoatCollider.scoreValue = BoatCollider.scoreValue + ((int)Random.Range(100,200)* (int)col.relativeVelocity.magnitude);
+			}else{
+				BoatCollider.scoreValue = BoatCollider.scoreValue + ((int)Random.Range(200,500)* (int)col.relativeVelocity.magnitude);
+			}
+				
+			//BoatCollider.scoreValue = BoatCollider.scoreValue + (10* (int)col.relativeVelocity.magnitude);
+			//t.text = "Score: " + score;
+			//Destroy(col.gameObject);
+
+        }
+    }
+}
