1e0c7b04ec185208d739793f3bff3e95b35f083b
Ricky Lu <daholmsey@gmail.com>
2019-04-29 23:40:53 -0400

    Throttles and transmissions with random controls added

---
+++ Assets/LeftTrans.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftTrans : MonoBehaviour
+{
+    public GameObject Cube;
+    public float maxtransmissionangle = 90.0f;
+    public float xangle;
+    Boolean rightmaxlock;
+    Boolean leftmaxlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightmaxlock = false;
+    }
+
+    // Update is called once per frame
+    // This sets up the transmission to move between -90 and 90 degrees
+    void Update()
+    {
+
+        xangle = Cube.transform.eulerAngles.x;
+        if (Input.GetKeyDown("l") && rightmaxlock == false)
+        {
+
+
+            Cube.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+
+
+            //  xangle += 90.0f;
+
+        }
+
+        if (Input.GetKeyDown("o") && leftmaxlock == false)
+        {
+
+            Cube.transform.Rotate(-90.0f, 0.0f, 0.0f, Space.Self);
+            //xangle -= 90.0f;
+
+        }
+        xangle = Cube.transform.localEulerAngles.x;
+        if (xangle == 90.0f)
+        {
+            rightmaxlock = true;
+        }
+        else
+        {
+            rightmaxlock = false;
+        }
+
+        if (xangle == 270.0f)
+        {
+            leftmaxlock = true;
+        }
+        else
+        {
+            leftmaxlock = false;
+        }
+        //xangle = Mathf.Clamp(xangle, -maxtransmissionangle, maxtransmissionangle);
+        /*
+        if(xangle==-180)
+        {
+            Cube.transform.localEulerAngles = new Vector3(90.0f, Cube.transform.localEulerAngles.y, Cube.transform.localEulerAngles.z);
+        }
+        if (xangle == -270)
+        {
+            Cube.transform.localEulerAngles = new Vector3(xangle, Cube.transform.localEulerAngles.y, Cube.transform.localEulerAngles.z);
+        }
+        */
+
+    }
+}
---
+++ Assets/RightThrottle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightThrottle : MonoBehaviour
+{
+    Boolean Rightmaxlock;
+    Boolean Leftmaxlock;
+    float rotatespeed = 45f;
+    public float xangle;
+    public int xiterations;
+    Boolean pulldown;
+    Boolean pullup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xiterations = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // xangle = transform.eulerAngles.x;
+        if (Input.GetKey(KeyCode.UpArrow) && Rightmaxlock == false)
+            transform.Rotate(rotatespeed * Time.deltaTime, 0.0f, 0.0f, Space.Self);
+
+
+        if (Input.GetKey(KeyCode.DownArrow) && Leftmaxlock == false)
+        {
+            transform.Rotate(-rotatespeed * Time.deltaTime, 0.0f, 0.0f, Space.Self);
+        }
+
+        xangle = transform.localEulerAngles.x;
+        if (xangle > 260)
+        {
+            pulldown = true;
+        }
+        else
+        {
+            pulldown = false;
+        }
+
+        if (xangle > 0 && xangle < 250)
+        {
+            pullup = true;
+        }
+        else
+        {
+            pullup = false;
+        }
+
+
+
+        if (xangle <= 270.8f && xiterations > 1 && pulldown)
+        {
+            Leftmaxlock = true;
+        }
+        else
+        {
+            Leftmaxlock = false;
+        }
+
+        if (xangle >= 89.0f && xiterations > 1 && pullup)
+        {
+            Rightmaxlock = true;
+        }
+        else
+        {
+            Rightmaxlock = false;
+        }
+        xiterations++;
+    }
+}
