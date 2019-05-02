5d6ab0b26e1a49f6ed5e663f3090dfe403af2623
odellla <odellla@live.unc.edu>
2019-05-02 01:17:23 -0400

    music and final dock

--- Assets/SteeringWheelSphere.cs
+++ Assets/SteeringWheelSphere.cs
@@ -6,9 +6,7 @@ using UnityEngine.UI;
 public class SteeringWheelSphere : MonoBehaviour
 {
     public Image SteeringWheel;
-
-
-    // Update is called once per frame
+	
     void Update()
     {
         Vector3 wheelpos = Camera.main.WorldToScreenPoint(this.transform.position);
