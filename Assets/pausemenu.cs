53fc10bb45b267daec0b02967d779b6046476faf
Ricky Lu <daholmsey@gmail.com>
2019-05-04 22:02:44 -0400

    Instruction controls screen on pressing h

---
+++ Assets/pausemenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool Gameispaused = false;
+
+    public GameObject pausemenuUI;
+
+    private void Start()
+    {
+        pausemenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            if(Gameispaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Gameispaused = false;
+    }
+
+    void Pause()
+    {
+        pausemenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Gameispaused = true;
+    }
+    
+}
